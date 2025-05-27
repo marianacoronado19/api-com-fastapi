9a050b4f2480870c5ff95d3fe18243845913104b
Mariana Coronado <maricoronad19@gmail.com>
2025-05-27 11:30:05 -0300

    criação database

--- data/must_watch.sql
+++ data/must_watch.sql
@@ -19,6 +19,9 @@
 -- Table structure for table `ator`
 --
 
+CREATE DATABASE IF NOT EXISTS `must_watch` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
+USE `must_watch`;
+
 DROP TABLE IF EXISTS `ator`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
